113ebdf800b3d250ecd9e259cc06017a9604ec27
Jon <jonwes2@gmail.com>
2018-06-26 19:32:22 -0500

    created a simple post for a new recipe

--- recipes/static/recipes/js/AddRecipeController.js
+++ recipes/static/recipes/js/AddRecipeController.js
@@ -3,7 +3,15 @@
     
     angular.module('recipes')
         .controller('AddRecipeController', ['$scope', '$http', function ($scope, $http) {
+            $scope.recipe = {};
             
+            $scope.addRecipe = function() {
+                var now = new Date();
+                $scope.recipe.created = now;
+                $scope.recipe.modified = now;
+                $scope.recipe.user = 1; // todo: find out how to get user id
+                $http.post('/recipes/recipes/', $scope.recipe);
+            };
         }
     ]);
 })();
