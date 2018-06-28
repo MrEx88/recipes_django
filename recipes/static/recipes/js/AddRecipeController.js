aaaa6f655d988512e14d4c80317efc5d4ae11c59
Jon <jonwes2@gmail.com>
2018-06-27 21:55:13 -0500

    trying to do a drag n drop for image

--- recipes/static/recipes/js/AddRecipeController.js
+++ recipes/static/recipes/js/AddRecipeController.js
@@ -5,11 +5,18 @@
         .controller('AddRecipeController', ['$scope', '$http', function ($scope, $http) {
             $scope.recipe = {};
             
+            $scope.image = null;
+            $scope.imageFileName = '';
+            
+            $scope.uploadme = {};
+            $scope.uploadme.src = '';
+            
             $scope.addRecipe = function() {
                 var now = new Date();
                 $scope.recipe.created = now;
                 $scope.recipe.modified = now;
                 $scope.recipe.user = 1; // todo: find out how to get user id
+                //$scope.recipe.base64Image = $scope.image; //see if we can send image this way
                 $http.post('/recipes/recipes/', $scope.recipe)
                     .then(function(response) {
                         //redirect to recipes
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,2 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Recipes
-
-
-def view(request, recipe_id):
-    recipe = get_object_or_404(Recipes, pk=recipe_id)
-    context = {'recipe': recipe}
-    return render(request, 'recipes/view.html', context)
