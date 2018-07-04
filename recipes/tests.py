e346f79677e3aa3d2fee9bf402692177f0a5b9ed
Jon <jonwes2@gmail.com>
2018-07-03 20:27:31 -0500

    added a mixin for logging in, changed assertEquals() to assertEqual() since it is deprecated, added more tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -5,13 +5,19 @@ from django.utils import timezone
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Recipes
+from .models import Bookmarks, Recipes
 
 
-class CreateRecipeTests(APITestCase):
+class UserMixin():
+    def create_user_and_login(self):
+        password = 'jonpassword'
+        self.superuser = User.objects.create_superuser('jon', 'me@me.com', password)
+        self.client.login(username=self.superuser.username, password=password)
+
+
+class CreateRecipeTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
         now = timezone.now()
         self.data = {
             'name': 'n',
@@ -26,56 +32,54 @@ class CreateRecipeTests(APITestCase):
 
     def test_can_create_recipe(self):
         response = self.client.post('/api/recipes/', self.data, format='json')
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_cant_create_invalid_recipe(self):
         data = {'name': 'n'}
         response = self.client.post('/api/recipes/', data, format='json')
-        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class ReadRecipeTests(APITestCase):
+class ReadRecipeTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
         now = timezone.now()
         self.recipe = Recipes.objects.create(name='chicken',
                                              ingredients='fried',
                                              instructions='steak',
                                              created=now,
                                              modified=now,
-                                             user_id=self.superuser.id)
+                                             user=self.superuser)
 
     def test_can_get_recipe(self):
         response = self.client.get('/api/recipes/{}/'.format(self.recipe.id))
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(response.data['id'], self.recipe.id)
-        self.assertEquals(response.data['name'], self.recipe.name)
-        self.assertEquals(response.data['ingredients'], self.recipe.ingredients)
-        self.assertEquals(response.data['instructions'], self.recipe.instructions)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.recipe.id)
+        self.assertEqual(response.data['name'], self.recipe.name)
+        self.assertEqual(response.data['ingredients'], self.recipe.ingredients)
+        self.assertEqual(response.data['instructions'], self.recipe.instructions)
 
     def test_cant_get_non_existent_recipe(self):
         response = self.client.get('/api/recipes/{}/'.format(self.recipe.id+1))
-        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-class UpdateRecipesTests(APITestCase):
+class UpdateRecipesTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
         self.now = timezone.now()
         self.recipe = Recipes.objects.create(name='chicken',
                                              ingredients='fried',
                                              instructions='steak',
                                              created=self.now,
                                              modified=self.now,
-                                             user_id=self.superuser.id)
+                                             user=self.superuser)
         self.data = {
             'name': 'n',
             'instructions': '1',
             'ingredients': '1',
-            'created': now,
-            'modified': now,
+            'created': self.now,
+            'modified': self.now,
             'user':
             self.superuser.id,
             'subRecipes': []
@@ -83,41 +87,39 @@ class UpdateRecipesTests(APITestCase):
 
     def test_can_update_recipe(self):
         response = self.client.put('/api/recipes/{}/'.format(self.recipe.id), self.data, format='json')
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         recipe = Recipes.objects.get(pk=self.recipe.id)
-        self.assertEquals(recipe.name, self.data['name'])
-        self.assertEquals(recipe.ingredients, self.data['ingredients'])
-        self.assertEquals(recipe.instructions, self.data['instructions'])
-        self.assertNotEquals(recipe.modified, self.now)
+        self.assertEqual(recipe.name, self.data['name'])
+        self.assertEqual(recipe.ingredients, self.data['ingredients'])
+        self.assertEqual(recipe.instructions, self.data['instructions'])
+        self.assertNotEqual(recipe.modified, self.now)
 
     def test_cant_update_non_existent_recipe(self):
         response = self.client.put('/api/recipes/{}/'.format(self.recipe.id+1))
-        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-class DeleteRecipesTests(APITestCase):
+class DeleteRecipesTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
         self.now = timezone.now()
         self.recipe = Recipes.objects.create(name='chicken',
                                              ingredients='fried',
                                              instructions='steak',
                                              created=self.now,
                                              modified=self.now,
-                                             user_id=self.superuser.id)
+                                             user=self.superuser)
 
     def test_can_delete_recipe(self):
         response = self.client.delete('/api/recipes/{}/'.format(self.recipe.id))
-        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         with self.assertRaises(Recipes.DoesNotExist):
             Recipes.objects.get(pk=self.recipe.id)
 
 
-class CreateAllRecipesTests(APITestCase):
+class CreateAllRecipesTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
         now = timezone.now()
         self.data = [{
             'name': 'n',
@@ -132,23 +134,21 @@ class CreateAllRecipesTests(APITestCase):
 
     def test_cant_create_all_recipes(self):
         response = self.client.post('/api/allRecipes/', self.data, format='json')
-        self.assertEquals(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-class ReadAllRecipesTests(APITestCase):
+class ReadAllRecipesTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
 
     def test_can_read_all_recipes(self):
         response = self.client.get('/api/allRecipes/')
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-class UpdateAllRecipesTests(APITestCase):
+class UpdateAllRecipesTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
         now = timezone.now()
         self.data = [{
             'name': 'n',
@@ -163,14 +163,47 @@ class UpdateAllRecipesTests(APITestCase):
 
     def test_cant_update_all_recipes(self):
         response = self.client.put('/api/allRecipes/', self.data, format='json')
-        self.assertEquals(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-class DeleteAllRecipesTests(APITestCase):
+class DeleteAllRecipesTests(APITestCase, UserMixin):
     def setUp(self):
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
-        self.client.login(username='john', password='johnpassword')
+        self.create_user_and_login()
 
     def test_cant_delete_all_recipes(self):
         response = self.client.delete('/api/allRecipes/')
-        self.assertEquals(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+class CreateBookmarksTests(APITestCase, UserMixin):
+    def setUp(self):
+        self.create_user_and_login()
+
+    def test_can_create_bookmark(self):
+        now = timezone.now()
+        data = {'name': 'b', 'url': 'path', 'created': now, 'modified': now, 'user': self.superuser.id}
+        response = self.client.post('/api/bookmarks/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_cant_create_an_invalid_bookmark(self):
+        data = {'name': 'b'}
+        response = self.client.post('/api/bookmarks/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ReadBookmarksTests(APITestCase, UserMixin):
+    def setUp(self):
+        self.create_user_and_login()
+        now = timezone.now()
+        self.bookmark = Bookmarks.objects.create(name='name',
+                                                 url='url',
+                                                 created=now,
+                                                 modified=now,
+                                                 user=self.superuser)
+
+    def test_can_read_bookmark(self):
+        response = self.client.get('/api/bookmarks/{}/'.format(self.bookmark.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_read_non_existent_bookmark(self):
+        response = self.client.get('/api/bookmarks/{}/'.format(self.bookmark.id+1))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
